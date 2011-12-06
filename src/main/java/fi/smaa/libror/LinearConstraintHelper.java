77816b151fce8ca7b75a2870f8ced6351d93ed7e
Tommi Tervonen <tervonen@ese.eur.nl>
2011-12-06 17:09:15 +0100

    Add linar constraint helper

---
+++ src/main/java/fi/smaa/libror/LinearConstraintHelper.java
@@ -0,0 +1,36 @@
+package fi.smaa.libror;
+
+import java.util.List;
+
+import org.apache.commons.math.optimization.linear.LinearConstraint;
+
+public class LinearConstraintHelper {
+	
+	public static String constraintToString(LinearConstraint c) {
+		StringBuffer sb = new StringBuffer();
+		for (int i=0;i<c.getCoefficients().getData().length;i++) {
+			if (i > 0) {
+				sb.append(" + ");
+			}
+			sb.append(c.getCoefficients().getData()[i]);			
+			sb.append(" * x");
+			sb.append(i);
+		}
+		sb.append(" ");
+		sb.append(c.getRelationship().toString());
+		sb.append(" ");
+		sb.append(c.getValue());
+		return sb.toString();
+	}
+	
+	public static void printConstraint(LinearConstraint c) {
+		System.out.println(constraintToString(c));
+	}
+	
+	public static void printConstraints(List<LinearConstraint> consts) {
+		for (LinearConstraint c : consts) {
+			printConstraint(c);
+			System.out.println("\n");
+		}
+	}
+}
