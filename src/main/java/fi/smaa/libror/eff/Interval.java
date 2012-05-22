27cda3051cd256d9a785c51f6cb62044bf4a8531
Tommi Tervonen <tervonen@ese.eur.nl>
2012-05-22 09:23:33 +0200

    Update license. Update DESCRIPTION. Update Makefile to clean also jar.

--- src/main/java/fi/smaa/libror/eff/Interval.java
+++ src/main/java/fi/smaa/libror/eff/Interval.java
@@ -1,23 +1,22 @@
 /*
-    This file is part of JSMAA.
-    JSMAA is distributed from http://smaa.fi/.
+ * This file is part of libror.
+ * libror is distributed from http://smaa.fi/libror
+ * Copyright (C) 2011-12 Tommi Tervonen.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-    (c) Tommi Tervonen, 2009-2010.
-    (c) Tommi Tervonen, Gert van Valkenhoef 2011.
-
-    JSMAA is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    JSMAA is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with JSMAA.  If not, see <http://www.gnu.org/licenses/>.
-*/
 package fi.smaa.libror.eff;
 
 import java.text.DecimalFormat;
