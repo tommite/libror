d1c942f9a4d67407c6548a30b0132fdf72ecaaa6
tommite <tervonen@ese.eur.nl>
2013-01-23 12:55:30 +0100

    Finish (?) B1-B5 constraints, B5 untested stil though

--- R-pkg/ror/R/etricror.R
+++ R-pkg/ror/R/etricror.R
@@ -4,15 +4,16 @@
 ##   where each row is a single assignment and the first column index of the
 ##   alternative, the second column index of the desired class
 ## necessary: whether to compute the necessary (T) or possible (F) relation
-etricror <- function(performances, profiles, assignments, necessary=TRUE) {
+etricror <- function(performances, profiles, assignments, necessary=TRUE, phi) {
   stopifnot(ncol(performances) == ncol(profiles))
   stopifnot(ncol(assignments) == 2)
+  stopifnot(length(phi) == ncol(performances))
   
   rel <- matrix(nrow=nrow(performances), ncol=nrow(profiles))
   
   for (i in 1:nrow(rel)) {
     for(j in 1:nrow(rel)) {
-      rel[i,j] = checkETRICRelation(performances, profiles, assignments, i, j, necessary=necessary)
+      rel[i,j] = checkETRICRelation(performances, profiles, assignments, i, j, necessary=necessary, phi=phi)
     }
   }
   
@@ -27,15 +28,15 @@ etricror <- function(performances, profiles, assignments, necessary=TRUE) {
 
 ## aIndex: index of the alternative
 ## cIndex: index of the category
-checkETRICRelation <- function(performances, profiles, assignments, aIndex, cIndex, necessary) {
+checkETRICRelation <- function(performances, profiles, assignments, aIndex, cIndex, necessary, phi) {
   stopifnot(is.logical(necessary))
   stopifnot(necessary) # only necessary implemented yet
 
-  baseVars <- buildBaseModelVariableMatrix(performances, profiles, assignments)
+  baseVars <- buildBaseModel(performances, profiles, assignments, phi)
   
 }
 
-buildBaseModelVariableMatrix <- function(performances, profiles, assignments) {
+buildBaseModel <- function(performances, profiles, assignments, phi) {
   nAlts <- nrow(performances)
   nCrit <- ncol(performances)
   nAssignments <- nrow(assignments)
@@ -45,7 +46,42 @@ buildBaseModelVariableMatrix <- function(performances, profiles, assignments) {
   b2 <- buildB2Constraint(nAlts, nCrit, nAssignments, nCats)
   b3 <- buildB3Constraint(nAlts, nCrit, nAssignments, nCats)
   b4 <- buildB4Constraint(nAlts, nCrit, nAssignments, nCats)
-  b5 <- buildB5Constraint(nAlts, nCrit, nAssignments, nCats)
+  b5 <- buildB5Constraint(performances, profiles, nAssignments, phi)
+
+  allConst <- combineConstraintsMatrix(b1, b2, b3, b4, b5)
+  colnames(allConst$lhs) <- getColNames(nalts, nCrit, nAssignments, nCats)
+  return(allConst)
+}
+
+getColNames <- function(nAlts, nCrit, nAssignments, nCats) {
+  res <- paste('w', 1:nCrit, sep='')
+
+  for (j in 1:nCrit) {
+    for (a in 1:nAlts) {
+      for (b in 1:nCats) {
+        res = c(res, paste('c', j, '(a', a, ',b', b, ')', sep=''))
+      }
+    }
+  }
+
+  for (j in 1:nCrit) {
+    for (a in 1:nAlts) {
+      for (b in 1:nCats) {
+        res = c(res, paste('c', j, '(b', a, ',a', b, ')', sep=''))
+      }
+    }
+  }
+
+  for (j in 1:nCrit) {
+    for (b in 1:(nCats-1)) {
+      res = c(res, paste('c', j, '(b', b, ',b', (b+1), ')', sep=''))
+    }
+  }
+
+  res = c(res, 'lam')
+  res = c(res, 'e')
+
+  return(res)
 }
 
 buildB1Constraint <- function(nAlts, nCrit, nAssignments, nCats) {
@@ -53,7 +89,13 @@ buildB1Constraint <- function(nAlts, nCrit, nAssignments, nCats) {
   for (j in 1 : nCrit) {
     lhs[getWjIndex(j)] = 1
   }
-  return(list(lhs=lhs, dir="=", rhs=c(1)))
+  lhs <- t(as.matrix(lhs))
+  dir <- as.matrix("=")
+  rhs <- as.matrix(c(0))
+  rownames(lhs) <- c("B1")
+  rownames(dir) <- c("B1")
+  rownames(rhs) <- c("B1")
+  return(list(lhs=lhs, dir=dir, rhs=rhs))
 }
 
 buildB2Constraint <- function(nAlts, nCrit, nAssignments, nCats) {
@@ -71,9 +113,15 @@ buildB2Constraint <- function(nAlts, nCrit, nAssignments, nCats) {
     lhs[getEpsilonIndex(nAlts, nCrit, nCats)] = 1
     ## -lambda
     lhs[getLambdaIndex(nAlts, nCrit, nCats)] = -1
+    lhs <- t(as.matrix(lhs))
+    rownames(lhs) <- c("B2")
     res <- rbind(res, lhs)
   }
-  return(list(lhs=res, dir=rep("<=", nCats), rhs=rep(0, tMinus1)))
+  dir <- as.matrix(rep("<=", tMinus1))
+  rownames(dir) <- rep("B2", tMinus1)
+  rhs <- as.matrix(rep(0, tMinus1))
+  rownames(rhs) <- rep("B2", tMinus1)
+  return(list(lhs=res, dir=dir, rhs=rhs))
 }
 
 buildB3Constraint <- function(nAlts, nCrit, nAssignments, nCats) {
@@ -82,8 +130,13 @@ buildB3Constraint <- function(nAlts, nCrit, nAssignments, nCats) {
   lhs2 <- rep(0, getNrBaseVars(nAlts, nCrit, nAssignments, nCats))
   lhs2[getLambdaIndex(nAlts, nCrit, nCats)] = 1
 
-  rhs = c(0.5, 1.0)
-  return(list(lhs=rbind(lhs1, lhs2), dir=c(">=", "<="), rhs=rhs))
+  lhs <- rbind(lhs1, lhs2)
+  rownames(lhs) <- c("B3", "B3")
+  rhs = as.matrix(c(0.5, 1.0))
+  rownames(rhs) <- c("B3", "B3")
+  dir <- as.matrix(c(">=", "<="))
+  rownames(dir) <- c("B3", "B3")
+  return(list(lhs=lhs, dir=dir, rhs=rhs))
 }
 
 buildB4Constraint <- function(nAlts, nCrit, nAssignments, nCats) {
@@ -102,7 +155,11 @@ buildB4Constraint <- function(nAlts, nCrit, nAssignments, nCats) {
     lhsRes <- rbind(lhsRes, lhs1, lhs2)
     rhsRes <- rbind(rhsRes, 0, 0.5)
   }
-  return(list(lhs=lhsRes, dir=rep("<=", nCrit*2), rhs=rhsRes))
+  rownames(lhsRes) <- rep("B4", nrow(lhsRes))
+  dir <- as.matrix(rep("<=", nCrit*2))
+  rownames(dir) <- rep("B4", nrow(dir))
+  rownames(rhsRes) <- rep("B4", nrow(rhsRes))
+  return(list(lhs=lhsRes, dir=dir, rhs=rhsRes))
 }
 
 ## phi is a vector of function objects (1 for each criterion)
@@ -143,7 +200,12 @@ buildB5Constraint <- function(performances, profiles, nAssignments, phi) {
       lhsRes <- rBind(lhsRes, lhs)
     }    
   }
-  return(list(lhs=lhsRes, dir=rep("=", nCrit*2), rhs=rep(0, nrRows)))
+  rownames(lhsRes) <- rep("B5", nrow(lhsRes))
+  dir <- as.matrix(rep("=", nCrit*2))
+  rownames(dir) <- rep("B5", nrow(dir))
+  rhs <- as.matrix(rep(0, nrRows))
+  rownames(rhs) <- rep("B5", nrow(rhs))
+  return(list(lhs=lhsRes, dir=dir, rhs=rhs))
 }
 
 getLambdaIndex <- function(nAlts, nCrit, nCats) {
@@ -186,4 +248,27 @@ getWjIndex <- function(j) {
 
 getNrBaseVars <- function(nAlts, nCrit, nAssignments, nCats) {
   return(getEpsilonIndex(nAlts=nAlts, nCrit=nCrit, nCats=nCats))
+}
+
+## p is preference threshold
+## q is indifference threshold
+buildPhi <- function(q, qMult = 0, p, pMult = 0, ascending=TRUE) {
+  stopifnot(p >= 0 && q >= 0 && p >= q)
+  return( function(x, y) { ## c(x, y)
+    diff <- y - x
+    if (ascending == FALSE) {
+      diff = 0 - diff
+    }
+
+    indif <- q + qMult * x
+    pref <- p + pMult * x
+
+    if (diff <= indif) {
+      return (1)
+    } else if (diff >= pref) {
+      return(0)
+    } else {
+      return ((diff - indif) / (pref - indif))
+    }
+  })
 }
